5c9ec9f025b4f8b4fd3f75512755a9539a932947
Rafael Fonseca <fonseca.wutang@gmail.com>
2022-02-06 20:33:16 +0000

    add botão play/pause

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("button");
+var audio = document.getElementById("myAudio");
+
+button.addEventListener("click", function(){
+    if(audio.paused){
+    audio.play();
+    button.innerHTML = "Pause";
+    } else {
+    audio.pause();
+    button.innerHTML = "Play";
+    }
+});
