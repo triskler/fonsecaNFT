7ca4c32502c016e5c34342317d2b3ae9e89dd534
Rafael Fonseca <fonseca.wutang@gmail.com>
2022-02-17 21:40:05 +0000

    add file and header

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-draw = function(){
-    console.log("hello world")
-}
+var botao = document.getElementsByTagName("button");
+
+var valores = [
+    "green",
+    "yellow",
+    "red",
+    "gray"
+];
+
+
+
+
+
+botao.addEventListener("click", function(){
+    botao.setAttribute(backgroundColor, valores[index])
+    
+});
