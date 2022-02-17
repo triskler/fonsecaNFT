7e558a23ae85b9953fe0e3e54efc9c527220e0ce
Rafael Fonseca <fonseca.wutang@gmail.com>
2022-02-17 22:23:00 +0000

    add files and first main

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-var botao = document.getElementsByTagName("button");
 
-var valores = [
-    "green",
-    "yellow",
-    "red",
-    "gray"
-];
-
-
-
-
-
-botao.addEventListener("click", function(){
-    botao.setAttribute(backgroundColor, valores[index])
-    
-});
