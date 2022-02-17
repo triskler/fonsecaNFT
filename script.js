e658f211522f899013e802e139cb926a0836ce78
Rafael Fonseca <fonseca.wutang@gmail.com>
2022-02-17 09:23:22 +0000

    att vscode

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-var button = document.getElementById("button");
-var audio = document.getElementById("myAudio");
-
-
-
-button.addEventListener("click", function(){
-    if(audio.paused){
-    audio.play();
-    button.innerHTML = "Pause";
-    } else {
-    audio.pause();
-    button.innerHTML = "Play";
-    }
-});
-
+draw = function(){
+    console.log("hello world")
+}
